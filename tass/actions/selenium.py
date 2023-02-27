bea3f7b83c6d39cd96a179580aaf3cd7bc4ec77f
Canadian-Ty <josephgillestyler.desjardins@statcan.gc.ca>
2023-02-27 11:32:13 -0500

    added finder function to actions method signature.

--- tass/actions/selenium.py
+++ tass/actions/selenium.py
@@ -2,48 +2,47 @@ import pathlib
 import os
 from selenium.common.exceptions import WebDriverException
 
+def _find_element(driver, element):
+    return driver.find_element(**element)
 
-def click(driver, *, element=None):
+
+def click(driver, find=_find_element, element=None):
     try:
-        _find_element(driver, element).click()
+        find(driver, element).click()
     except WebDriverException as e:
         print("Exception: ", e, " trying again")
-        _find_element(driver, element).click()
+        find(driver, element).click()
 
 
-def type(driver, *, element=None, text=''):
+def type(driver, find=_find_element, element=None, text=''):
     try:
-        _find_element(driver, element).send_keys(text)
+        find(driver, element).send_keys(text)
     except WebDriverException as e:
         print("Exception: ", e, " trying again")
-        _find_element(driver, element).send_keys(text)
+        find(driver, element).send_keys(text)
 
 
-def load_url(driver, *, url=''):
+def load_url(driver, url=''):
     driver.get(url)
 
 
-def load_file(driver, *, relative_path=''):
+def load_file(driver, relative_path=''):
     url = os.path.join(pathlib.Path().resolve(), relative_path)
     driver.get('file://' + url)
 
 
-def read_attribute(driver, *, element=None, attribute=''):
+def read_attribute(driver, find=_find_element, element=None, attribute=''):
     try:
-        return _find_element(driver, element).get_attribute(attribute)
+        return find(driver, element).get_attribute(attribute)
 
     except WebDriverException as e:
         print("Exception: ", e, " trying again")
-        return _find_element(driver, element).get_attribute(attribute)
+        return find(driver, element).get_attribute(attribute)
 
 
-def read_css(driver, *, element=None, attribute=''):
+def read_css(driver, find=_find_element, element=None, attribute=''):
     try:
-        return _find_element(driver, element).value_of_css_property(attribute)
+        return find(driver, element).value_of_css_property(attribute)
     except WebDriverException as e:
         print("Exception: ", e, " trying again")
-        return _find_element(driver, element).value_of_css_property(attribute)
-
-
-def _find_element(driver, element):
-    return driver.find_element(*element)
+        return find(driver, element).value_of_css_property(attribute)
