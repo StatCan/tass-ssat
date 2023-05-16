3fea7af941552f2a83872df3230ab049e04a2537
Dominic Parent <dominic.parent@statcan.gc.ca>
2023-05-16 08:36:56 -0400

    Added test data.

--- src/tass/tools/conf.py
+++ src/tass/tools/conf.py
@@ -3,5 +3,5 @@ import openpyxl
 def convert(path):
     wb = openpyxl.load_workbook(path)
     ws = wb.active
-    print("Total row: " + ws.max_row)
-    print("Total column: " + ws.max_column)
+    print("Total row: " + str(ws.max_row))
+    print("Total column: " + str(ws.max_column))
