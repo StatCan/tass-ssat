d47f28c9c81dad38ce8bb380e6318c98acb1a764
Dominic Parent <dominic.parent@statcan.gc.ca>
2023-05-23 10:53:05 -0400

    Updated .gitignore with macOS system files and created first version of convert tool.

--- src/tass/tools/conf.py
+++ src/tass/tools/conf.py
@@ -1,7 +1,32 @@
 import openpyxl
 
 def convert(path):
+
+    conf_file = {}
     wb = openpyxl.load_workbook(path)
     ws = wb.active
-    print("Total row: " + str(ws.max_row))
-    print("Total column: " + str(ws.max_column))
+
+    header = []
+    for cell in ws[1]:
+        header.append(cell.value)
+
+    conf_file["steps"] = []
+
+    for row in ws.iter_rows(min_row=2):
+        steps = {}
+        index = 0
+        while index < len(header):
+            
+            steps[header[index]] = row[index].value
+
+            index += 1
+
+        conf_file["steps"].append(steps)
+
+    print(conf_file)
+
+def main():
+    convert('./tests/data/testfile.xlsx')
+
+if __name__ == '__main__':
+    main()
