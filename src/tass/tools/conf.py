c8ddbd1b9d5381ded16a363139b5d3551b6892bd
Dominic Parent <dominic.parent@statcan.gc.ca>
2023-06-28 14:34:51 -0400

    Updated data model and created first version of conversion script from xlsx to json.

--- src/tass/tools/conf.py
+++ src/tass/tools/conf.py
@@ -5,28 +5,91 @@ def convert(path):
     Helper tool to convert xlsx config file to a in-memory json equivalent.
     """
     conf_file = {}
+    conf_file["Test_runs"] = []
+    conf_file["Test_suites"] = []
+    conf_file["Test_cases"] = []
+    conf_file["Steps"] = []
     wb = openpyxl.load_workbook(path) # Open conf file.
-    ws = wb.active # First worksheet holds conf info.
 
-    header = []
-    for cell in ws[1]: # Assumes that first row is header info.
-        header.append(cell.value)
+    test_run = []
+    test_suite = []
+    test_case = []
+    test_steps = []
 
-    conf_file['steps'] = []
+    for sheet in wb.sheetnames:
+        
+        test_type = wb[sheet]['A1'].value
 
-    # Loop goes through entire xlsx file from 2nd row onwards.
-    # Each row is assumed to be a "step" in a config file.
-    # Relevant information is extracted and assigned to a value
-    # pulled from the header.
-    for row in ws.iter_rows(min_row=2):
-        steps = {}
-        index = 0
-        while index < len(header):
-            
-            steps[header[index]] = row[index].value
+        if test_type == 'tr_uuid:':
+            test_run.append(sheet)
+        elif test_type == 'ts_uuid:':
+            test_suite.append(sheet)
+        elif test_type == 'tc_uuid:':
+            test_case.append(sheet)
+        else:
+            print('Not a tass Excel template.')
+    
+    print(test_run)
+    print(test_suite)
+    print(test_case)
 
-            index += 1
+    if test_run:
+        conf_file = convert_test_run(test_run, conf_file, wb)
+    if test_suite:
+        conf_file = convert_test_suite(test_suite, conf_file, wb)
+    if test_case:
+        conf_file = convert_test_case(test_case, conf_file, wb)
+    print("Printing conf file")
+    print(conf_file)
 
-        conf_file['steps'].append(steps)
+def convert_test_case(test_case, conf, wb):
+    conf["Test_cases"] = []
+    for case in test_case:
+        tc = {}
+        tc["tc_uuid"] = wb[case]['B1'].value
+        tc["title"] = wb[case]['D1'].value
+        tc["steps"] = []
+        for row in wb[case].iter_rows(min_row=3, max_col=4):
+            steps = {}
+            tc["steps"].append(row[0].value)
+            steps["uuid"] = row[0].value
+            steps["title"] = row[1].value
+            steps["action"] = row[2].value
+            steps["parameter"] = row[3].value
+            conf["Steps"].append(steps)
 
-    return conf_file
+        conf["Test_cases"].append(tc)
+    return conf
+
+def convert_test_suite(test_suite, conf, wb):
+    conf["Test_suites"] = []
+    for suite in test_suite:
+        ts = {}
+        ts["uuid"] = wb[suite]['B1'].value
+        ts["test_cases"] = []
+        ts["keywords"] = []
+        for row in wb[suite].iter_rows(min_row=2, min_col=2, max_col=2):
+            ts["test_cases"].append(row[0].value)
+        for row in wb[suite].iter_rows(min_row=1, min_col=4, max_col=4):
+            ts["keywords"].append(row[0].value)
+        conf["Test_suites"].append(ts)
+
+    return conf
+
+def convert_test_run(test_run, conf, wb):
+    conf["Test_runs"] = []
+    for run in test_run:
+        tr = {}
+        tr["uuid"] = wb[run]['B1'].value
+        tr["build"] = wb[run]['B2'].value
+        tr["start_time"] = wb[run]['D1'].value
+        tr["end_time"] = wb[run]['D2'].value
+        tr["test_cases"] = []
+        tr["test_suites"] = []
+        for row in wb[run].iter_rows(min_row=3, min_col=2, max_col=2):
+            tr["test_suites"].append(row[0].value)
+        for row in wb[run].iter_rows(min_row=3, min_col=4, max_col=4):
+            tr["test_cases"].append(row[0].value)
+        conf["Test_runs"].append(tr)
+
+    return conf
