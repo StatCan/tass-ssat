690ac8f846a1b66a9bb73621fb890fb2d7d8c87f
Desjardins <josephgillestyler.desjardins@statcan.gc.ca>
2023-10-31 14:08:55 -0400

    Added function _handle_response

--- src/tass/tools/testrail/testrail_apiclient.py
+++ src/tass/tools/testrail/testrail_apiclient.py
@@ -13,19 +13,34 @@ class TestRailAPIClient(APIClient):
         super().__init__(base_url,
                          auth,
                          ssl_verification_level)
+                         
+    def _handle_response(self, response):
+        with response:
+            if (response.status_code == 200):
+                return response.json()
+            elif (response.status_code >= 400):
+                # TODO: report error
+                return {"status": f"{response.status_code}: {response.reason}",
+                        "message": response.text['error']}
+                }
+            elif:
+                # TODO: unexpected response, implementation?
+                # Unexpected response
+                return response
+            
 
     def get(self, endpoint):
         _request = self.start_request(endpoint, method="GET")
         _request.headers['Content-Type'] = 'application/json'
         response = self.send_request(_request)
-        return response
+        return self._handle_response(response)           
 
     def post(self, endpoint, payload):
         _request = self.start_request(endpoint, method="POST")
         _request.headers['Content-Type'] = 'application/json'
         _request.data = bytes(json.dumps(payload or {}), 'utf-8')
         response = self.send_request(_request)
-        return response        
+        return self._handle_response(response)   
 
     def post_attachment(self, endpoint, attachment):
         _request = self.start_request(endpoint, method='POST')
@@ -34,4 +49,4 @@ class TestRailAPIClient(APIClient):
             _attachment = {'attachment': f}
         _request.files = _attachment
         response = self.send_request(_request)
-        return response
+        return self._handle_response(response)
