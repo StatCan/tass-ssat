c2aa0978a7f2dc35f05a5c88852ce4b292328112
Canadian-Ty <josephgillestyler.desjardins@statcan.gc.ca>
2023-11-24 15:50:07 -0500

    completed base implementation for Excel load and get.

--- src/tass/secrets/excel.py
+++ src/tass/secrets/excel.py
@@ -1,23 +1,36 @@
 import openpyxl
 import tass.secrets.secrets as secrets
 from pathlib import Path
+from collections import namedtuple
 
 
 class Excel(secrets.DataSource):
 
     def __init__(self, config):
-        super.__init__(config)
+        super().__init__(config)
     
-    def _load_config(self, config_path):
-        config = super()._load_config(config_path)
-        source_path = Path(config['source']['path']).resolve()
-        self._source = openpyxl.Workbook(source_path)
+
+    def _load_datasource(self, config):
+        _source_path = Path(config['source']['path']).resolve()
+        _source = config['source']
+        _all_collections = config['collections']
+
+        _collections = [_c for _c in _all_collections if _c['name'] in _source['collections']] 
+
+
+        self._collections = self._load_collections(
+                                openpyxl.load_workbook(_source_path),
+                                _collections, config['entry-sets'])
         
         
-    def _load_collections(self, collections):
+    def _load_collections(self, source, collections, entry_sets):
+        _loaded = {}
         for coll in collections:
             _name = coll['name']
-            _entry_set = coll['entry-set']
+            _entry_set = [se for se in entry_sets if se['name'] == coll['entry-set']][0]
+            _wsheet = source[_name]
+            _loaded[_name] = Excel.Sheet(_wsheet, _entry_set)
+        return _loaded
             
         
 
@@ -26,22 +39,24 @@ class Excel(secrets.DataSource):
             super().__init__(collection=collection, entry_set=entry_set)
             
 
-        def _load_entry_set(self, entry_set):
+        def _load_entries(self, collection, entry_set):
+            breakpoint()
             self._columns = []
-            self._name = entry_set['name']
+            self._name = collection.title
             _key = entry_set['key']
             _cols = entry_set['columns']
+            _has_headers = entry_set['has-headers']
             ColumnDefinition = namedtuple("ColumnDefinition", "name, column")
             for col in _cols:
                 self._columns.append(ColumnDefinition(col['name'], col['column']))
             
-            start_row = 2 if has_headers else 1
+            start_row = 2 if _has_headers else 1
             _entries = {}
             for index, row in enumerate(collection.iter_rows(min_row=start_row), start_row):
                 entry = {"row": index}
                 for col in self._columns:
-                    entry[col.name] = row[col.column]
-                _entries[_key] = Excel.Row(entry)
+                    entry[col.name] = row[col.column].value
+                _entries[entry[_key]] = Excel.Row(entry)
             
             return _entries
             
--- src/tass/secrets/secrets.py
+++ src/tass/secrets/secrets.py
@@ -4,8 +4,18 @@ from pathlib import Path
 
 class DataSource():
 
-    def __init__(self, config):
-        self._config = config
+    def __init__(self, config_path):
+        __path = Path(config_path).resolve()
+        self._config_path = __path
+        with open(__path) as conf:
+            config = json.load(conf)
+        self._load_datasource(config)
+
+    def _load_datasource(self, config):
+        raise NotImplementedError("Please implement this function.")
+
+    def from_collection(self, collection):
+        return self._collections[collection]
         
     
 class Collection():
@@ -15,6 +25,31 @@ class Collection():
     def _load_entries(self, *args, **kwargs):
         raise NotImplementedError("Please implement this function.")
 
+    def select(self, where='key', comparison=None, value='', count=1):
+        if where == 'key':
+            return self._entries[value]
+        else:
+
+            def equals(actual, expected):
+                return actual == expected
+
+            matches = []
+            match comparison:
+                case 'equals' | 'e':
+                    compare = equals 
+
+            for key, entry in self._entries.items():
+                if compare(entry.get(where), value):
+                    if count == 1:
+                        return entry
+                    else:
+                        matches.append(entry)
+
+
+
 class Entry():
     def __init__(self, data):
         self._data = data
+
+    def get(self, key):
+        return self._data[key]
