7a721ae49a339b64f68fda77d854e3face40b082
Canadian-Ty <josephgillestyler.desjardins@statcan.gc.ca>
2024-02-29 14:20:11 -0500

    Refactored tass converter using the new namespace

---
+++ tass-converter/tests/test_jsonschema.py
@@ -0,0 +1,22 @@
+import unittest
+import json
+from jsonschema import Draft7Validator
+
+
+class TestJsonSchema(unittest.TestCase):
+    def setUp(self):
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        print("Beginning new test TestCase %s" % self._testMethodName)
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+        with open("templates/templates.json") as f:
+            self.schema = json.load(f)
+
+    def test_JsonSchemaValidate(self):
+        print(self.schema)
+        print("Schema validator result:")
+        print(Draft7Validator.check_schema(self.schema))
+
+
+if __name__ == '__main__':
+    unittest.main()
