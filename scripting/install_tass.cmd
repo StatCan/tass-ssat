bcb545a7ab0b76b846400e9132af82b08ff0170b
Parent <dominic.parent@statcan.gc.ca>
2024-07-05 15:11:08 -0400

    Create first version of CMD install script. Doesn't actually work...

---
+++ scripting/install_tass.cmd
@@ -0,0 +1,25 @@
+
+@echo "CLONE START"
+git clone https://github.com/StatCan/tass-ssat.git
+@echo "CLONE END"
+@echo "CD TO TASS-SSAT"
+cd tass-ssat
+@echo "SUCCESS CD?"
+@echo "CREATE CONDA ENV"
+conda env create -f environment.yaml
+@echo "END CREATE CONDA ENV"
+@echo "CONDA ACTIVATE"
+conda activate tass_ssat-dev
+@echo "END CONDA ACTIVATE"
+@echo "CD TASS-BASE"
+cd tass-base
+@echo "SUCCESS CD?"
+@echo "INSTALL EDITABLE TASS-BASE"
+pip install -e .
+@echo "INSTALL SUCCESS?"
+@echo "CD TO TASS CONVERTER"
+cd ..\tass-converter
+@echo "SUCCESS CD?
+@echo "INSTALL EDITABLE TASS-CONVERTER"
+pip install -e.
+@echo "INSTALL SUCCESS"
