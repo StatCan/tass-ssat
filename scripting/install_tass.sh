5be3fcc5c502269bf2e990a2d0a3df61a25c8b98
Dominic Parent <dominic.parent@mailbox.org>
2024-07-05 13:51:43 -0400

    Created install script for dev version of tass.

---
+++ scripting/install_tass.sh
@@ -0,0 +1,35 @@
+#! /bin/zsh
+
+POSITIONAL=()
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -e|--environment)
+            PACKAGE_ENV="${2}"
+            shift
+            shift
+            ;;
+esac
+done
+set -- "${POSITIONAL[@]}"
+
+if [[ $1 == 'help' || $1 == '-h' || $1 == '--help' ]]
+then
+    echo "Usage:"
+    echo "install_tass.sh -e pip_or_conda_package"
+    exit 0
+fi
+
+CWD=$(pwd)
+
+git clone https://github.com/StatCan/tass-ssat.git
+cd tass-ssat
+conda env create -f environment.yaml
+eval "$(conda shell.bash hook)"
+conda activate tass_ssat-dev
+cd tass-base
+pip install -e .
+cd ../tass-converter
+pip install -e .
