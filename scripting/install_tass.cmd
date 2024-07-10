62ecc0a0eac6f6297f964e3b49a65a5e641543f1
DominicParent <33664844+DominicParent@users.noreply.github.com>
2024-07-10 11:11:33 -0400

    Update install_tass.cmd to remove debug messages.

--- scripting/install_tass.cmd
+++ scripting/install_tass.cmd
@@ -1,25 +1,14 @@
 @echo off
-@echo "CLONE START"
+@echo "CLONING TASS REPOSITORY"
 git clone https://github.com/StatCan/tass-ssat.git
-@echo "CLONE END"
-@echo "CD TO TASS-SSAT"
 cd tass-ssat
-@echo "SUCCESS CD?"
-@echo "CREATE CONDA ENV"
+@echo "CREATING CONDA ENVIRONMENT"
 call conda env create -f environment.yaml
-@echo "END CREATE CONDA ENV"
-@echo "CONDA ACTIVATE"
 call conda activate tass_ssat-dev
-@echo "END CONDA ACTIVATE"
-@echo "CD TASS-BASE"
 cd tass-base
-@echo "SUCCESS CD?"
-@echo "INSTALL EDITABLE TASS-BASE"
+@echo "INSTALLING EDITABLE TASS-BASE"
 pip install -e .
-@echo "INSTALL SUCCESS?"
-@echo "CD TO TASS CONVERTER"
 cd ..\tass-converter
-@echo "SUCCESS CD?
-@echo "INSTALL EDITABLE TASS-CONVERTER"
+@echo "INSTALLING EDITABLE TASS-CONVERTER"
 pip install -e.
-@echo "INSTALL SUCCESS"
+@echo "INSTALLATION COMPLETED"
