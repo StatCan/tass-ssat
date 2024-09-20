affa0913582cc78165cd8a6abd33c47b9ee0f59e
Dominic Parent <dominic.parent@statcan.gc.ca>
2024-09-20 14:54:41 -0400

    Laying out some test code.

--- tass-orchestrator/src/tass/orchestrator/__main__.py
+++ tass-orchestrator/src/tass/orchestrator/__main__.py
@@ -1,1 +1,11 @@
-print("This ran?")
+import argparse
+from . import core
+
+def main(file):
+	core.execute(file)
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("--file", "-f", required=True)
+	main(**vars(parser.parse_args()))
