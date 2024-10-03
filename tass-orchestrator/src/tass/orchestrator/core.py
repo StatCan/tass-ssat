98aa6f541f0843228a7d16afb204f75eef357e29
Dominic Parent <dominic.parent@statcan.gc.ca>
2024-10-03 11:33:50 -0400

    Initial json processing for orchestrator.

--- tass-orchestrator/src/tass/orchestrator/core.py
+++ tass-orchestrator/src/tass/orchestrator/core.py
@@ -1,4 +1,31 @@
 import json
 
-def execute():
+def execute(file):
 	print("Executor!")
+	print(file)
+	try:
+		f = open(file)
+	except IOError as e:
+		print(e)
+		return
+	with f:
+		orch = json.load(f)
+
+	for flow in orch["Test_flow"]:
+		print("######## Test flow #########")
+		print(flow)
+
+		for conversion in flow["conversion"]:
+			print("######### test conversion ############")
+			print(conversion)
+
+		for executor in flow["executor"]:
+			print("########## test executor ############")
+			print(executor)
+
+		for reporter in flow["reporter"]:
+			print("############ test reporter ##############")
+			print(reporter)
+
+	print("######## Test orch #########")
+	print(orch)
