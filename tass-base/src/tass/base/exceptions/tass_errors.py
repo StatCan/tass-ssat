3eef7ba4ece0156cf33e05dccd6d1c6e54f3b788
Desjardins <josephgillestyler.desjardins@statcan.gc.ca>
2024-10-29 11:51:51 -0400

    Added some error handling.

--- tass-base/src/tass/base/exceptions/tass_errors.py
+++ tass-base/src/tass/base/exceptions/tass_errors.py
@@ -1,18 +1,28 @@
-class TassError(Exception):
-    def __init__(self, message, reason, *args):
-        super().__init__()
-        self._message = message
-        self._reason = reason
-        self._args = args
+class TassException(Exception):
+    def __init__(self, message, *args):
+        super().__init__(message, *args)
 
     @property
     def message(self):
-        return self._message
+        return self.args[0].format(*self.args[1:])
+        
 
-    @property
-    def reason(self):
-        return self._reason
 
-    @property
-    def args(self):
-        return self._args
+class TassUUIDEexception(TassError):
+    def __init__(self, message, *args):
+        super().__init__(message, *args)
+
+
+class TassUUIDNotFound(TassUUIDEexception):
+    def __init__(self, uuid):
+        message = "Given UUID: {} was not found."
+        super().__init__(message, uuid)
+
+
+class TassAmbiguousUUID(TassUUIDEexception):
+    def __init__(self, uuid):
+        message = "Given UUID: {} is ambiguous. Associated value is not unique."
+        super().__init__(message, uuid)
+        
+
+    
--- tass-base/src/tass/base/schema/parser.py
+++ tass-base/src/tass/base/schema/parser.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+from tass.base.exceptions.tass_errors import TassUUIDException, TassUUIDNotFound, TassAmbiguousUUID
 from tass.base.log.logging import getLogger
 from tass.report.registrar import ReporterRegistrar
 from ..actions.action_manager import get_manager
@@ -40,18 +43,23 @@ class Tass1Parser(Parser):
             try:
                 browser = self._parse_browser(case['browser_uuid'], job)
                 steps = self._parse_steps(case['steps'], job)
-            except
+            except Tass
 
     def _parse_steps(self, steps, job):
         all_steps = job['Steps']
         step_config = []
         for uuid in steps:
-            found = list(filter(lambda x: uuid == x['uuid'], all_steps))
+            found = list(filter(lambda step: uuid == step['uuid'], all_steps))
             if len(found)>1:
-                self.log.warning("Ambigous step configuration selected. Choosing first match.")
+                self.log.warning("Ambiguous step configuration selected. Checking compatibility")
+                if not all(step == found[0] for step in found[1:]):
+                    self.log.warning("Unable to resolve ambiguous uuids.")
+                    raise TassAmbiguousUUID(uuid)
             elif len(found) == 0:
-                self.log.warning("No matching step configuration found. Skipping this case.")
-                continue
+                self.log.warning("No matching step configuration found.")
+                raise TassUUIDNotFound(uuid)
+
+            return deepcopy(found[0])
 
     def _parse_reporters(self, job):
         reporters = job['Reporters']
@@ -70,8 +78,12 @@ class Tass1Parser(Parser):
         self.log.debug("Using browsers: %s", browser)
         found =  list(filter(lambda b: b['uuid'] in browser, job["browsers"]))
         if len(found)>1:
-            self.log.warning("Ambigous browser configuration selected. Choosing first match.")
+            self.log.warning("Ambiguous browser configuration selected. Attempting to resolve.")
+            if not all(step == found[0] for step in found[1:]):
+                self.log.warning("Unable to resolve ambiguous uuids.")
+                raise TassAmbiguousUUID(uuid)
         elif len(found) == 0:
-            self.log.warning("No matching browser configuration found. Skipping this browser.")
-            return None
-        return found[0]
+            self.log.warning("No matching browser configuration found.")
+            raise TassUUIDNotFound(uuid)
+
+        return deepcopy(found[0])
