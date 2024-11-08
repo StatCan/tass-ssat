988639ebcaebdf76ea84fe6b46494a3180ecf0d5
Dominic Parent <dominic.parent@statcan.gc.ca>
2024-11-08 08:43:22 -0500

    Updated package requirements and removed dev print statements.

--- tass-orchestrator/src/tass/orchestrator/core.py
+++ tass-orchestrator/src/tass/orchestrator/core.py
@@ -4,25 +4,19 @@ from tass.converter import __main__ as tass_conv
 from tass.base import __main__ as tass_base
 
 def run_conversion(conversion):
-	print("######### test conversion ############")
-	print(conversion)
 	tass_conv.main(conversion["conv-prop"]["convert"],
 		           conversion["conv-prop"]["source"],
 		           conversion["conv-prop"]["target"])
 
 def run_executor(executor):
-	print("########## test executor ############")
-	print(executor)
 	tass_base.main(executor["base-prop"]["file_path"],
 		           executor["base-prop"]["no_validate"])
 
 def run_reporter(reporter):
-	print("############ test reporter ##############")
-	print("TASS reporter module not currently implemented.")
+	# TASS reporter module not currently implemented.
+	pass
 
 def run_flows(file):
-	print("Orchestrator!")
-	print(file)
 	try:
 		f = open(Path(file).resolve())
 	except IOError as e:
@@ -32,9 +26,6 @@ def run_flows(file):
 		orch = json.load(f)
 
 	for flow in orch["Test_flow"]:
-		print("######## Test flow #########")
-		print(flow)
-
 		for conversion in flow["conversion"]:
 			run_conversion(conversion)
 
@@ -43,6 +34,3 @@ def run_flows(file):
 
 		for reporter in flow["reporter"]:
 			run_reporter(reporter)
-
-	print("######## Test orch #########")
-	print(orch)
