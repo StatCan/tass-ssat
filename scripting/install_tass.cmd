56d6444833526e9d390edd2e830c917fc79e45d2
Dominic Parent <dominic.parent@statcan.gc.ca>
2024-12-16 09:49:12 -0500

    Updated install scripts with new packages.

--- scripting/install_tass.cmd
+++ scripting/install_tass.cmd
@@ -5,10 +5,16 @@ cd tass-ssat
 @echo "CREATING CONDA ENVIRONMENT"
 call conda env create -f environment.yaml
 call conda activate tass_ssat-dev
-cd tass-base
+cd \tass-report
+@echo "INSTALLING EDITABLE TASS-REPORT"
+pip install -e .
+cd ..\tass-base
 @echo "INSTALLING EDITABLE TASS-BASE"
 pip install -e .
 cd ..\tass-converter
 @echo "INSTALLING EDITABLE TASS-CONVERTER"
-pip install -e.
+pip install -e .
+cd ..\tass-orchestrator
+@echo "INSTALLING EDITABLE TASS-ORCHESTRATOR"
+pip install -e .
 @echo "INSTALLATION COMPLETED"
