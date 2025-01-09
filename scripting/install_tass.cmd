8796dbf1fd4bf417cca29b22870b34a7af627a72
Dominic Parent <dominic.parent@statcan.gc.ca>
2025-01-09 13:54:01 -0500

    Fixed script issue for Windows.

--- scripting/install_tass.cmd
+++ scripting/install_tass.cmd
@@ -5,16 +5,12 @@ cd tass-ssat
 @echo "CREATING CONDA ENVIRONMENT"
 call conda env create -f environment.yaml
 call conda activate tass_ssat-dev
-cd \tass-report
 @echo "INSTALLING EDITABLE TASS-REPORT"
-pip install -e .
-cd ..\tass-base
+pip install -e tass-report
 @echo "INSTALLING EDITABLE TASS-BASE"
-pip install -e .
-cd ..\tass-converter
+pip install -e tass-base
 @echo "INSTALLING EDITABLE TASS-CONVERTER"
-pip install -e .
-cd ..\tass-orchestrator
+pip install -e tass-converter
 @echo "INSTALLING EDITABLE TASS-ORCHESTRATOR"
-pip install -e .
+pip install -e tass-orchestrator
 @echo "INSTALLATION COMPLETED"
