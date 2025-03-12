9fbf8ad1adda9038b3e7ce7cd4776b3a205063a5
Tyler Desjardins <117662482+ty-desj@users.noreply.github.com>
2025-03-12 10:44:58 -0400

    Renamed tass.base

---
+++ tass-core/src/tass/core/schema/validate.py
@@ -0,0 +1,29 @@
+from tass.core.log.logging import getLogger
+from packaging.version import Version
+from tass.core.schema import validator
+
+
+log = getLogger(__name__)
+
+
+DEFAULT = validator.Tass1Validator
+DEFAULT_SCHEMA = "0.0.0"
+
+
+def validate(job, validate_on):
+    log.info("Validating job file.")
+    schema_version = job.get('schema-version', DEFAULT_SCHEMA)
+    log.info("Using schema version: %s", schema_version)
+
+    version = Version(schema_version)
+
+    if version >= Version('0.0.0'):
+        log.info("Validating schema against schema version: 1.0.0")
+        schema = validator.Tass1Validator()
+    else:
+        schema = DEFAULT()
+
+    if validate_on:
+        schema.validate(job)
+    log.info("Validation successful.")
+    return schema.parser()
