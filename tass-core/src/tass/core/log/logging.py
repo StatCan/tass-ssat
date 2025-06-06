6e58673d57c4aea625ebede1bc89a0357baccdea
Tyler Desjardins <117662482+ty-desj@users.noreply.github.com>
2025-06-06 06:46:28 -0400

    Initial commit for logging implementation.

--- tass-core/src/tass/core/log/logging.py
+++ tass-core/src/tass/core/log/logging.py
@@ -3,66 +3,60 @@ import logging.config
 import json
 from pathlib import Path
 
-Path("./log").mkdir(parents=True, exist_ok=True)
-CONFIG_PATH = "configs/log-config.json"
 
-path = Path(CONFIG_PATH)
-
-if not path.exists():
-    config = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "simple": {
-                "format": "%(asctime)s - %(levelname)s >>> %(message)s",
-                "datefmt": "%d-%m-%y -- %H:%M:%S"
-            },
-            "detailed": {
-                "format": (
-                    "%(asctime)s - %(name)s:"
-                    "%(funcName)s:%(lineno)s -- "
-                    "%(levelname)s >>> %(message)s"
-                    )
-            }
+log_fldr = None
+DEFAULT_PATH = "./logs"
+DEFAULT_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "%(asctime)s - %(levelname)s >>> %(message)s",
+            "datefmt": "%d-%m-%y -- %H:%M:%S"
         },
-        "handlers": {
-            "cli": {
-                "class": "logging.StreamHandler",
-                "level": "INFO",
-                "formatter": "simple"
-            },
-            "info-file": {
-                "class": "logging.handlers.TimedRotatingFileHandler",
-                "filename": "log/tass.log",
-                "when": "W0",
-                "level": "INFO",
-                "formatter": "simple"
-            },
-            "debug-file": {
-                "class": "logging.handlers.TimedRotatingFileHandler",
-                "filename": "log/tass-debug.log",
-                "when": "D",
-                "level": "DEBUG",
-                "formatter": "detailed"
-            }
+        "detailed": {
+            "format": (
+                "%(asctime)s - %(name)s:"
+                "%(funcName)s:%(lineno)s -- "
+                "%(levelname)s >>> %(message)s"
+                )
+        }
+    },
+    "handlers": {
+        "cli": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "simple"
         },
-        "loggers": {
-            "tass": {
-                "handlers": ["cli", "info-file", "debug-file"],
-                "level": "DEBUG"
-            }
+        "info-file": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "filename": f"{log_fldr}/tass.log",
+            "when": "W0",
+            "level": "INFO",
+            "formatter": "simple"
+        },
+        "debug-file": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "filename": f"{log_fldr}/tass-debug.log",
+            "when": "D",
+            "level": "DEBUG",
+            "formatter": "detailed"
+        }
+    },
+    "loggers": {
+        "tass": {
+            "handlers": ["cli", "info-file", "debug-file"],
+            "level": "DEBUG"
         }
     }
+}
 
-    path.parent.mkdir(parents=True, exist_ok=True)
-    with open(path, 'w', encoding='utf-8') as f:
-        json.dump(config, f, indent=4)
-
-else:
-    with open(path) as f:
-        config = json.load(f)
-
-logging.config.dictConfig(config)
+def init_logger(path=DEFAULT_PATH, config=DEFAULT_CONFIG):
+    # Create log folder
+    _path = Path(path)
+    if not path.endswith("log"):
+        _path.joinpath("log")
+    _path.mkdir(parents=True, exist_ok=True)
 
 
 def getLogger(*name):
