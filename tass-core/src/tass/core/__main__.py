3989f48abc1cbf33090d3038d08092bdf84c2819
Desjardins <josephgillestyler.desjardins@statcan.gc.ca>
2025-10-07 08:07:53 -0400

    Standardized tass-core main entry point.

--- tass-core/src/tass/core/__main__.py
+++ tass-core/src/tass/core/__main__.py
@@ -3,62 +3,17 @@ import json
 from pathlib import Path
 from .schema.parse import parse
 from .log.logging import getLogger
+from .execute import execute
 
 
 log = getLogger(__name__)
-
-
-class TassEncoder(json.JSONEncoder):
-    # Convert Python objects to JSON equivalent.
-    # TODO: Update format to match test management tool
-    def default(self, obj):
-        """
-        Default JSON encoder
-        for custom TASS classes.
-        Serializable TASS classes should
-        implement the toJson function.
-        """
-        if (hasattr(obj, 'toJson')):
-            return obj.toJson()
-        else:
-            raise TypeError(
-                "Unserializable object {} of type {}".format(obj, type(obj))
-                )
 
 
 def main(file_path, no_validate):
     """
     Starting point for execution of tests.
     """
-    log.info("\n\n <<<<<< TASS Starting >>>>>> \n\n")
-
-    path = Path(file_path).resolve()
-
-    run = parse(path, no_validate)
-
-    log.info("<<<<< Starting Run: %s >>>>>", run.uuid)
-    for case in run.collect():
-        log.info("")
-        log.info("< < < Starting Case: %s > > >", case.uuid)
-        log.info("")
-
-        case.execute_tass()
-
-        log.info("")
-        log.info("> > > Finished Case: %s < < <", case.uuid)
-        log.info("")
-
-    Path('results').mkdir(exist_ok=True)
-
-    file_name = run.uuid + '---' + run.start_time + '.json'
-    result_path = Path().resolve() / "results" / file_name
-    try:
-        f = open(result_path, 'w+', encoding='utf-8')
-    except IOError as e:
-        log.error("An IOError occured: %s" % e)
-        return
-    with f:
-        json.dump(run, f, indent=4, cls=TassEncoder)
+    execute(file_path, no_validate)
 
 
 if __name__ == '__main__':
@@ -66,7 +21,7 @@ if __name__ == '__main__':
     # automated browser testing tool parser
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--file', "-f",
+    parser.add_argument('--file', "-f", dest="file_path",
                         action='store', required=True)
 
     parser.add_argument('--no-validate', action='store_false',
@@ -74,4 +29,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     log.debug("Launch arguments:", vars(args))
-    main(args.file, args.no_validate)
+    main(**vars(args))
