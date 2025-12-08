3674adba1c4ad1666e7eb0a115ab056738cd9ea0
Desjardins <josephgillestyler.desjardins@statcan.gc.ca>
2025-12-08 09:47:50 -0500

    Added custom hide keyboard function. Implemented Android strategy for hide keyboard. Added helper fucntion to switch android contexts

--- tass-core/src/tass/core/drivers/mobile/customdrivers.py
+++ tass-core/src/tass/core/drivers/mobile/customdrivers.py
@@ -1,3 +1,5 @@
+import time
+import enum
 from appium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from ...log.logging import getLogger
@@ -45,6 +47,9 @@ class MobileDriver(webdriver.Remote):
 
 class AndroidDriver(MobileDriver):
 
+    NATIVE = "NATIVE_APP"
+    CHROME = "WEBVIEW_chrome"
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -53,6 +58,74 @@ class AndroidDriver(MobileDriver):
         self.logger.debug("Android driver found element >>> tag: %s, location: %s",
                           element.tag_name, rect)
         return element
+
+    def hide_keyboard(self, strategy="back", *args, **kwargs):
+        def back(*args, **kwargs):
+            # Hiding the keyboard by tapping the android back button
+            if self.is_keyboard_shown:
+                # If keyboard is not displayed, do not use the "back" button
+                self.logger.debug("Virtual keyboard is not displayed")
+                return
+            # Save the current context to switch back quickly
+            curr = self.current_context
+            self.logger.debug("Current context is: %s", curr)
+            if curr != self.NATIVE:
+                # Switch to NATIVE context to interact with android back button
+                self.logger.debug("Switching to %s context", self.NATIVE)
+                self.switch_to_context(self.NATIVE)
+            
+            self.logger.info("Closing virtual keyboard with back button")
+            self.back()
+
+            if curr != self.current_context:
+                # Return to the original context for ease of use.
+                self.logger.debug("Switching back to %s context", curr)
+                self.switch_to_context(curr)
+        
+        def default(*args, **kwargs):
+            # Use the default android hide_keyboard strategy.
+            super().hide_keyboard()
+
+        valid_strategies = {
+            "back": back,
+            "default": default
+        }
+
+
+        hide = valid_strategies.get(strategy, None)
+        if not hide:
+            self.logger.warning("\"%s\" is not a valid strategy. Using \"back\" strategy.", strategy)
+            hide = back
+        hide(*args, **kwargs)
+
+        if self.is_keyboard_shown:
+            self.logger.warning("Keyboard failed to close")
+            return False
+        else:
+            self.logger.info("Virtual keyboard close")
+            return True
+         
+
+
+    def find_webview_context(self):
+        for _ in range(5):
+            for ctx in self.contexts:
+                if "WEBVIEW" in ctx:
+                    return ctx
+            time.sleep(0.5)
+        # TODO: handle edgecase where no Webview context present
+
+    def switch_to_context(self, context):
+        found = False
+        for _ in range(5):
+            if context in self.contexts:
+                found = True
+                break
+            else:
+                time.sleep(0.5)
+        # TODO: handle edgecase where context not present
+        if found:
+            self.switch_to.context(context)
 
 class IOSDriver(MobileDriver):
 
