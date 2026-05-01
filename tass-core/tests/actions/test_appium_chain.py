607fa2b573f0da1ff46c21524454fd0a9c66a1f1
Tyler <tyler@Phils-Mac-mini.local>
2026-05-01 11:04:50 -0400

    Implemented more appium chain functions. Appium Chain will not be further supported at this time.

--- tass-core/tests/actions/test_appium_chain.py
+++ tass-core/tests/actions/test_appium_chain.py
@@ -11,6 +11,8 @@ class TestAppiumChain(TestAppium):
         for device, driver in self.appium_starter(self.drivers):
             try:
                 with self.subTest(device=device[1].__name__):
+                    breakpoint()
+                    
                     self.load_initial_url(driver, url)
                     self.close_nav(driver)
                     locator = self.pages['btn-page']['elements']['btn']
@@ -29,20 +31,20 @@ class TestAppiumChain(TestAppium):
                     driver.quit()
 
     def test_AppiumChainWriteElement(self):
-        url = self.pages['input-page']['url']
+        url = self.pages['txt-page']['url']
         for device, driver in self.appium_starter(self.drivers):
             try:
                 with self.subTest(device=device[1].__name__):
                     self.load_initial_url(driver, url)
                     self.close_nav(driver)
                     text = "testing"
-                    locator = self.pages['input-page']['elements']['input']
+                    locator = self.pages['txt-page']['elements']['input']
                     chain.write(driver, locator=locator, text=text)
                     self.assertTrue(bool(
                         driver.chain().w3c_actions.devices[0].actions))
                     chain.perform(driver)
-                    locator = {"by": "xpath", "value": f"//*[@id='{self.pages['input-page']['elements']['input']['value']}']"}
-                    actual = driver().find_element(**locator).get_attribute("value")
+                    locator = {"by": "xpath", "value": f"//*[@id='{locator['value']}']"}
+                    actual = driver().find_element(**locator).get_property("value")
                     self.assertEqual(actual, text)
             finally:
                 if driver:
@@ -72,16 +74,37 @@ class TestAppiumChain(TestAppium):
                     driver.quit()
 
     def test_AppiumChainReset(self):
-        pass
+        url = self.pages['btn-page']['url']
+        for device, driver in self.appium_starter(self.drivers):
+            try:
+                with self.subTest(device=device[1].__name__):
+                    self.load_initial_url(driver, url)
+                    self.close_nav(driver)
+                    locator = self.pages['btn-page']['elements']['btn']
+                    chain.move_mouse(driver, locator=locator)
+                    chain.click(driver)
+                    self.assertTrue(bool(
+                        driver.chain().w3c_actions.devices[0].actions))
+                    chain.reset(driver)
+                    self.assertFalse(bool(
+                        driver.chain().w3c_actions.devices[0].actions))
+            finally:
+                if driver:
+                    driver.quit()
+                    
 
     def test_AppiumChainMoveTo(self):
+        # TODO: How to test this function?
         pass
 
     def test_AppiumChainScrollByAmount(self):
+        # TODO: How to test this function?
         pass
 
     def test_AppiumChainScrollToElement(self):
+        # TODO: How to test this function?
         pass
 
     def test_AppiumChainScrollFromOrigin(self):
+        # TODO: How to test this function?
         pass
