8d923b614baad6c6a3f3edf9d23dcf8a3974bccd
Tyler Desjardins <117662482+ty-desj@users.noreply.github.com>
2026-07-16 12:51:48 -0400

    Updated Version checking for validation of job files. (#296)

--- tass-core/src/tass/core/parser/validate.py
+++ tass-core/src/tass/core/parser/validate.py
@@ -1,4 +1,5 @@
 from tass.core.log.logging import getLogger
+from packaging.specifiers import SpecifierSet
 from packaging.version import Version
 from tass.core.parser import validator
 
@@ -9,21 +10,23 @@ log = getLogger(__name__)
 DEFAULT = validator.Tass1_1Validator
 DEFAULT_SCHEMA = "1.1.0"
 
+def version_check(version, spec):
+    return Version(version) in SpecifierSet(spec)
+
 
 def validate(job, validate_on):
     log.info("Validating job file.")
     schema_version = job.get('schema-version', DEFAULT_SCHEMA)
     log.info("Using schema version: %s", schema_version)
 
-    version = Version(schema_version)
-
-    if version <= Version("1.0.0"):
+    if version_check(schema_version, "<1.1"):
         log.info("Validating schema against schema version: 1.0.0")
         schema = validator.Tass1Validator()
-    elif version >= Version('1.1.0'):
+    elif version_check(schema_version, "~=1.1"):
         log.info("Validating schema against schema version: 1.1.0")
         schema = validator.Tass1_1Validator()
     else:
+        log.warning("Invalid schema version. Attempting default schema validation: {}", DEFAULT_SCHEMA)
         schema = DEFAULT()
 
     if validate_on:
